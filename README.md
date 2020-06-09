3c6c4570dede6895ded7f53d4842e79c360ebe56
Stian Skjong <Stian.Skjong@sintef.no>
2020-06-10 00:48:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+[_![MSVC Conan](https://github.com/sintef-ocean/conan-ogre3d/workflows/MSVC%20Conan/badge.svg)_](https://github.com/sintef-ocean/conan-ogre3d/actions?query=workflow%3A%22MSVC+Conan%22)
+[_![GCC Conan](https://github.com/sintef-ocean/conan-ogre3d/workflows/GCC%20Conan/badge.svg)_](https://github.com/sintef-ocean/conan-ogre3d/actions?query=workflow%3A%22GCC+Conan%22)
+[_![Clang Conan](https://github.com/sintef-ocean/conan-ogre3d/workflows/Clang%20Conan/badge.svg)_](https://github.com/sintef-ocean/conan-ogre3d/actions?query=workflow%3A%22Clang+Conan%22)
+[ ![Download](https://api.bintray.com/packages/sintef-ocean/conan/ogre3d%3Asintef/images/download.svg) ](https://bintray.com/sintef-ocean/conan/ogre3d%3Asintef/_latestVersion)
+
+
+The recipe generates library packages, which can be found at [Bintray](https://bintray.com/sintef-ocean/conan/ogre3d%3Asintef/_latestVersion).
+The package is usually consumed using the `conan install` command or a *conanfile.txt*.
+
+## How to use this package
+
+1. Add remote to conan's package [registry.txt](http://docs.conan.io/en/latest/reference/config_files/registry.txt.html):
+
+   ```bash
+   $ conan remote add sintef https://api.bintray.com/conan/sintef-ocean/conan
+   ```
+
+2. Using *conanfile.txt* in your project with *cmake*
+
+   Add a [*conanfile.txt*](http://docs.conan.io/en/latest/reference/conanfile_txt.html) to your project. This file describes dependencies and your configuration of choice, e.g.:
+
+   ```
+   [requires]
+   ogre3d/[>=0.1]@sintef/stable
+
+   [options]
+   ogre3d:shared=False # by default
+
+   [imports]
+   licenses, * -> ./licenses @ folder=True
+
+   [generators]
+   cmake
+   ```
+
+   Insert into your *CMakeLists.txt* something like the following lines:
+   ```cmake
+   cmake_minimum_required(VERSION 3.13)
+   project(TheProject CXX)
+
+   include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+   conan_basic_setup()
+
+   add_executable(the_executor code.cpp)
+   target_link_libraries(the_executor i${CONAN_LIBS})
+   ```
+   Then, do
+   ```bash
+   $ mkdir build && cd build
+   $ conan install .. -b missing -s build_type=<build_type>
+   ```
+   where `<build_type>` is e.g. `Debug` or `Release`.
+   You can now continue with the usual dance with cmake commands for configuration and compilation. For details on how to use conan, please consult [Conan.io docs](http://docs.conan.io/en/latest/)
+
+## Package options
+
+| Option            | Allowed values    |   Default value   |
+| ----------------- | ----------------- | ----------------- |
+| with_cg           | [True, False]     | False             |
+| with_boost        | [True, False]     | False             |
+| with_poco         | [True, False]     | False             |
+| samples           | [True, False]     | False             |
+| with_python       | [True, False]     | False             |
+| with_csharp       | [True, False]     | False             |
+| with_java         | [True, False]     | False             |
+| bites             | [True, False]     | False             |
+| direct3d9_renderer| [True, False]     | False             |
+| opengl_renderer   | [True, False]     | False             |
+| opengl3_renderer  | [True, False]     | False             |
+| opengles_renderer | [True, False]     | False             |
+
+## Known recipe issues
+
+None
---
+++ conanfile.py
@@ -0,0 +1,184 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class Ogre3dConan(ConanFile):
+    name = "ogre3d"
+    version = "1.12.5"
+    license = "MIT"
+    author = "konrad"
+    url = "https://github.com/KonradNoTantoo/ogre3d_conan"
+    description = "3D graphics rendering engine"
+    topics = ("graphics", "3D rendering", "3D", "conan")
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {
+        "with_cg": [True, False],
+        "with_boost": [True, False],
+        "with_poco": [True, False],
+        "samples": [True, False],
+        "with_python": [True, False],
+        "with_csharp": [True, False],
+        "with_java": [True, False],
+        "bites": [True, False],
+        "direct3d9_renderer": [True, False],
+        "direct3d11_renderer": [True, False],
+        "opengl_renderer": [True, False],
+        "opengl3_renderer": [True, False],
+        "opengles_renderer": [True, False],
+        }
+
+    default_options = {
+        "with_cg": False,
+        "with_boost": False,
+        "with_poco": False,
+        "samples": False,
+        "with_python": False,
+        "with_csharp": False,
+        "with_java": False,
+        "bites": False,
+        "direct3d9_renderer": False,
+        "direct3d11_renderer": False,
+        "opengl_renderer": False,
+        "opengl3_renderer": False,
+        "opengles_renderer": False,
+        }
+
+    generators = "cmake"
+
+    requires = [
+        ("bzip2/1.0.8@conan/stable", "override"),
+        ("libpng/1.6.37@bincrafters/stable", "override"),
+        ("freetype/2.10.0@bincrafters/stable"),
+        ("zlib/1.2.11@conan/stable"),
+        ("pugixml/1.10@bincrafters/stable"),
+        ("sdl2/2.0.10@bincrafters/stable"),
+        ("zziplib/0.13.69@utopia/testing"),
+        # ("ois/1.5@utopia/testing"), # for older versions
+        ("freeimage/3.18.0@utopia/testing"),
+    ]
+
+    folder_name = "ogre-{}".format(version)
+
+    # using scm is a workaround for https://github.com/OGRECave/ogre/issues/1332
+    scm = {
+        "type": "git",
+        "subfolder": folder_name,
+        "url": "https://github.com/OGRECave/ogre.git",
+        "revision": "v{}".format(version),
+        "submodule": "recursive"
+    }
+
+
+    def configure(self):
+        # we only need sdl for IO control
+        self.options["sdl2"].fPIC = False
+        self.options["sdl2"].iconv = False
+        self.options["sdl2"].sdl2main = False
+
+        if self.settings.os == "Linux":
+            self.options["sdl2"].alsa = False
+            self.options["sdl2"].jack = False
+            self.options["sdl2"].pulse = False
+            self.options["sdl2"].nas = False
+            self.options["sdl2"].xcursor = False
+            self.options["sdl2"].xinerama = False
+            self.options["sdl2"].xinput = False
+            self.options["sdl2"].xrandr = False
+            self.options["sdl2"].xscrnsaver = False
+            self.options["sdl2"].xshape = False
+            self.options["sdl2"].xvm = False
+
+        if self.settings.os != "Windows":
+            del self.options.direct3d9_renderer
+            del self.options.direct3d11_renderer
+
+
+    def requirements(self):
+        if self.options.with_boost:
+            self.requires("boost/1.71.0@conan/stable")
+
+        if self.options.with_poco:
+            self.requires("poco/1.9.4")
+
+        if self.options.with_cg:
+            self.requires("nvidia-cg-toolkit-binaries/3.1.0013@utopia/testing")
+
+        if self.settings.os == "Linux" and self.options.bites:
+            self.requires("libxaw/1.0.13@bincrafters/stable")
+
+
+    def source(self):
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.folder_name), "project(OGRE VERSION 1.12.5)",
+                              '''project(OGRE VERSION 1.12.5)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
+link_libraries(${CONAN_LIBS})
+add_compile_definitions(GLEW_NO_GLU)''')
+
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+
+        cmake.definitions["OGRE_BUILD_DEPENDENCIES"] = "NO" # use libraries built by conan
+
+        cmake.definitions["OGRE_COPY_DEPENDENCIES"] = "OFF"
+        cmake.definitions["OGRE_INSTALL_DEPENDENCIES"] = "OFF"
+        cmake.definitions["OGRE_INSTALL_PDB"] = "ON"
+        cmake.definitions["OGRE_BUILD_PLUGIN_CG"] = "ON" if self.options.with_cg else "OFF"
+        cmake.definitions["OGRE_BUILD_SAMPLES"] = "ON" if self.options.samples else "OFF"
+        cmake.definitions["OGRE_BUILD_COMPONENT_PYTHON"] = "ON" if self.options.with_python else "OFF"
+        cmake.definitions["OGRE_BUILD_COMPONENT_CSHARP"] = "ON" if self.options.with_csharp else "OFF"
+        cmake.definitions["OGRE_BUILD_COMPONENT_JAVA"] = "ON" if self.options.with_java else "OFF"
+        cmake.definitions["OGRE_BUILD_COMPONENT_BITES"] = "ON" if self.options.bites else "OFF"
+
+        if self.settings.os == "Windows":
+            cmake.definitions["OGRE_BUILD_RENDERSYSTEM_D3D9"] = "ON" if self.options.direct3d9_renderer else "OFF"
+            cmake.definitions["OGRE_BUILD_RENDERSYSTEM_D3D11"] = "ON" if self.options.direct3d11_renderer else "OFF"
+
+        cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GL3PLUS"] = "ON" if self.options.opengl_renderer else "OFF"
+        cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GL"] = "ON" if self.options.opengl3_renderer else "OFF"
+        cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GLES2"] = "ON" if self.options.opengles_renderer else "OFF"
+
+        cmake.configure(source_folder=self.folder_name)
+        return cmake
+
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+
+
+    def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+
+
+    def package_info(self):
+        libs = [
+            "OgreMain",
+            "OgreOverlay",
+            "OgrePaging",
+            "OgreProperty",
+            "OgreRTShaderSystem",
+            "OgreTerrain",
+            "OgreVolume",
+        ]
+
+        if self.options.bites: libs.append("OgreBites")
+
+        self.cpp_info.includedirs.extend([
+            "include/OGRE",
+            "include/OGRE/Overlay",
+            "include/OGRE/Paging",
+            "include/OGRE/Property",
+            "include/OGRE/RTShaderSystem",
+            "include/OGRE/Terrain",
+            "include/OGRE/Volume",
+        ])
+        if self.options.bites: self.cpp_info.includedirs.append("include/OGRE/Bites")
+
+        if self.settings.compiler == "Visual Studio" and self.settings.build_type == "Debug":
+            self.cpp_info.libs = [lib + "_d" for lib in libs]
+        else:
+            self.cpp_info.libs = libs
