748e6b8125696748b551267338a2a3ed7a7fe94e
Joakim Haugen <joakim.haugen@gmail.com>
2020-06-12 12:57:01 +0200

    Only add openmp flag for clang 10

--- conanfile.py
+++ conanfile.py
@@ -135,7 +135,7 @@ add_compile_definitions(GLEW_NO_GLU)'''.format(self.version))
         cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GL3PLUS"] = "ON" if self.options.opengl_renderer else "OFF"
         cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GL"] = "ON" if self.options.opengl3_renderer else "OFF"
         cmake.definitions["OGRE_BUILD_RENDERSYSTEM_GLES2"] = "ON" if self.options.opengles_renderer else "OFF"
-        if self.settings.compiler == "clang":
+        if self.settings.compiler == "clang" and self.settings.compiler.version == "10":
             cmake.definitions["CMAKE_EXE_LINKER_FLAGS"] = "-fopenmp=libomp"
 
         cmake.configure(source_folder=self.folder_name)
@@ -175,7 +175,7 @@ add_compile_definitions(GLEW_NO_GLU)'''.format(self.version))
         if self.options.bites:
             self.cpp_info.includedirs.append("include/OGRE/Bites")
 
-        if self.settings.compiler == "clang":
+        if self.settings.compiler == "clang" and self.settings.compiler.version == "10":
             self.cpp_info.exelinkflags = ["-fopenmp=libomp"]
 
         if self.settings.compiler == "Visual Studio" and self.settings.build_type == "Debug":
