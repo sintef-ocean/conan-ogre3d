7288b7034a97a3de4667625a2e5b9458fb152aca
Joakim Haugen <joakim.haugen@sintef.no>
2023-04-11 15:20:12 +0200

    Migrate recipe to conan v2
      - Add v13.6.4
      - Remove patches for versions 13.3.4 and 13.4.0
      - Update CI workflows
      - Fix syntax error workflows
      - Build some troublesome libs
      - Fix build issues with old cmake<3.16

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,31 +1,26 @@
-import os
-
-from conans import ConanFile, CMake, tools
+from os import path
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 
 
 class Ogre3dTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_paths", "cmake_find_package"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
-        if not tools.cross_building(self.settings):
-            target_name = "test_package"
-            if self.settings.os == "Windows":
-                tester_exe = target_name + ".exe"
-                tester_path = os.path.join(self.build_folder,
-                                           str(self.settings.build_type))
-            else:
-                tester_exe = target_name
-                tester_path = "." + os.sep
-            self.run(os.path.join(tester_path, tester_exe),
-                     run_environment=True)
+        if can_run(self):
+            bin_path = path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
